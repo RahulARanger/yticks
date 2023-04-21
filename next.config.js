652498396b11d4fc048dc6dde5f845876348f5e6
Rahul <saihanumarahul66@gmail.com>
2023-04-21 18:50:42 +0530

    Fixed some superimposed css

--- next.config.js
+++ next.config.js
@@ -2,17 +2,17 @@ const constants = require("next/constants")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
 
-module.exports = (phase, {defaultConfig}) => {
+module.exports = (phase, { defaultConfig }) => {
   let extras = {};
-  
-  if(process.env.ANALYZE && phase === constants.PHASE_DEVELOPMENT_SERVER){
+
+  if (process.env.ANALYZE && phase === constants.PHASE_DEVELOPMENT_SERVER) {
     const withNextBundleAnalyzer =
-    require('next-bundle-analyzer')({clientOnly: false});
+      require('next-bundle-analyzer')({ clientOnly: false });
     extras = withNextBundleAnalyzer(nextConfig);
   }
 
-  return {reactStrictMode: true, ...defaultConfig, ...extras};
+  return { ...nextConfig, ...defaultConfig, ...extras };
 }
