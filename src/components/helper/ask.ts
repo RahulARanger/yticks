5ceb02ad2853908ccbfcdd552efae0e7a3b7750f
Rahul <saihanumarahul66@gmail.com>
2023-05-01 22:09:09 +0530

    Fixed the issue for the mock data
    It is required when we are using some sensitive keys

--- src/components/helper/ask.ts
+++ src/components/helper/ask.ts
@@ -5,7 +5,7 @@ import { askButRead } from "./generalRequest";
 import { ExpectedVideoDetails } from "@/pages/api/data/videoById";
 import { ExpectedCommentThread } from "@/pages/api/data/commentThreads";
 
-const isMock = process.env.IS_DEV ? "mock" : "data";
+const isMock = process.env.NEXT_PUBLIC_IS_DEV ? "mock" : "data";
 
 interface SWRResponse<Details> {
     data?: Details;
