ba0d934f25bfa51cd8dbdcecd4ff17096a6c74f3
Rahul <saihanumarahul66@gmail.com>
2023-06-23 22:25:45 +0530

    playlist is marked as in progress

--- src/pages/video/playlist.tsx
+++ src/pages/video/playlist.tsx
@@ -1,66 +1,70 @@
-import { VideoPlayListHeader } from "@/components/header";
-import { Component, ReactNode } from "react";
-import Head from "next/head";
-import { withRouter } from "next/router";
-import { WithRouterProps } from "next/dist/client/with-router";
-import { PlayListViewState } from "@/components/types/playlist";
-import DetailedPageView from "@/components/Playlist/DetailedPageView";
-import { decodeID } from "../api/data/playList";
+// import { VideoPlayListHeader } from "@/components/header";
+// import { Component, ReactNode } from "react";
+// import Head from "next/head";
+// import { withRouter } from "next/router";
+// import { WithRouterProps } from "next/dist/client/with-router";
+// import { PlayListViewState } from "@/components/types/playlist";
+// import DetailedPageView from "@/components/Playlist/DetailedPageView";
+// import { decodeID } from "../api/data/playList";
 
-class Playlist extends Component<WithRouterProps, PlayListViewState> {
-    state: PlayListViewState = {
-        listID: "",
-        videoID: "",
-    };
+// class Playlist extends Component<WithRouterProps, PlayListViewState> {
+//     state: PlayListViewState = {
+//         listID: "",
+//         videoID: "",
+//     };
 
-    resetSearch() {
-        this.props.router.push({
-            pathname: this.props.router.pathname,
-            query: {},
-        });
-        this.setState({ listID: "", videoID: "" });
-    }
+//     resetSearch() {
+//         this.props.router.push({
+//             pathname: this.props.router.pathname,
+//             query: {},
+//         });
+//         this.setState({ listID: "", videoID: "" });
+//     }
 
-    toggleSettings() {}
+//     toggleSettings() {}
 
-    handleSearch(rawID: string) {
-        this.props.router.push({
-            pathname: this.props.router.pathname,
-            query: { id: rawID },
-        });
-        const [videoID, listID] = decodeID(rawID);
-        this.setState({ videoID, listID });
-    }
+//     handleSearch(rawID: string) {
+//         this.props.router.push({
+//             pathname: this.props.router.pathname,
+//             query: { id: rawID },
+//         });
+//         const [videoID, listID] = decodeID(rawID);
+//         this.setState({ videoID, listID });
+//     }
 
-    render(): ReactNode {
-        return (
-            <>
-                <Head>
-                    <title>Search Playlist by ID</title>
-                </Head>
-                <VideoPlayListHeader
-                    textSearched={this.state.listID}
-                    title="YTicks"
-                    resetSearch={this.resetSearch.bind(this)}
-                    onSettingsRequest={this.toggleSettings.bind(this)}
-                    requested={String(
-                        this.props.router.query.q ||
-                            this.props.router.query.id ||
-                            ""
-                    )}
-                    onSearch={this.handleSearch.bind(this)}
-                />
-                {this.state.listID ? (
-                    <DetailedPageView
-                        videoID={this.state.videoID}
-                        listID={this.state.listID}
-                    />
-                ) : (
-                    <></>
-                )}
-            </>
-        );
-    }
+//     render(): ReactNode {
+//         return (
+//             <>
+//                 <Head>
+//                     <title>Search Playlist by ID</title>
+//                 </Head>
+//                 <VideoPlayListHeader
+//                     textSearched={this.state.listID}
+//                     title="YTicks"
+//                     resetSearch={this.resetSearch.bind(this)}
+//                     onSettingsRequest={this.toggleSettings.bind(this)}
+//                     requested={String(
+//                         this.props.router.query.q ||
+//                             this.props.router.query.id ||
+//                             ""
+//                     )}
+//                     onSearch={this.handleSearch.bind(this)}
+//                 />
+//                 {this.state.listID ? (
+//                     <DetailedPageView
+//                         videoID={this.state.videoID}
+//                         listID={this.state.listID}
+//                     />
+//                 ) : (
+//                     <></>
+//                 )}
+//             </>
+//         );
+//     }
+// }
+
+// export default withRouter(Playlist);
+
+export default function needToWait() {
+    return <>In Progress</>;
 }
-
-export default withRouter(Playlist);
